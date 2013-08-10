65cf871bef364d7089a7e1288eeed57c297e6cbe
Andrew <andrew@bluehousegroup.com>
2013-08-09 20:16:03 -0400

    adding command to create yml schema files directly from a database

---
+++ tools/import_db.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+	This function is mean't to read the structure of a database table or 
+	an entire database and create yml files for Dinkly
+
+	Example Usages: 
+		php tools/import_db.php -s=my_database
+		php tools/import_db.php -s=my_database -m=my_table
+*/
+
+require_once('config/bootstrap.php');
+
+//get the options to use
+$options = getopt("s:m::");
+$model_name = null;
+if(isset($options['m'])) $model_name = $options['m'];
+
+//call the main function
+getDbStructure($options['s'], $model_name);
+
+
+//this function theoretically would be moved into Dinkly core when completed
+function getDbStructure($schema, $model_name = null, $verbose_output = true, $override_database_name = null)
+{
+	DinklyDataConfig::setActiveConnection($schema);
+
+	//Use the proper DB credentials, or apply a passed-in override
+	$creds = DinklyDataConfig::getDBCreds();
+	$db_name = $creds['DB_NAME'];
+	if($override_database_name) { $db_name = $override_database_name; }
+
+	//Create database if it doesn't exist
+	$host = "localhost";
+	$db = new PDO("mysql:host=".$creds['DB_HOST'].";dbname=".$db_name."", $creds['DB_USER'], $creds['DB_PASS']);
+
+	$db_name = DinklyBuilder::sanitize($db, $db_name);
+	$model_name = DinklyBuilder::sanitize($db, $model_name);
+
+	//get columns from specified table or all tables
+	if($model_name)
+	{
+		$stmt = $db->prepare("SHOW COLUMNS FROM " . $model_name . "");
+		$stmt->execute();	
+		$table_schema = $stmt->fetchAll();
+		echo '\nRead table ' . $model_name . ' columns...\n';
+
+		sqlToYml($table_schema, $model_name, $db_name);
+	}
+	else
+	{
+		$stmt = $db->prepare("SHOW TABLES");
+		$stmt->execute();
+		$table_names = $stmt->fetch();
+
+		foreach($table_names as $key => $table_name)
+		{
+			if(is_numeric($key))
+			{
+				$stmt = $db->prepare("SHOW COLUMNS FROM " . $table_name . "");
+				$stmt->execute();	
+				$table_schema = $stmt->fetchAll();
+				echo '\nRead table ' . $table_name . ' columns...\n';
+
+				sqlToYml($table_schema, $table_name, $db_name);
+			}
+		}
+	}
+
+
+}
+
+function sqlToYml($sql, $model_name, $database_name)
+{
+	$output = "";
+	$model_name_camel = DinklyBase::convertToCamelCase($model_name, 1);
+
+	// Output the table name
+	$output .= "table_name: " . $model_name . "\nregistry:\n";
+
+	// Loop over the result set
+	foreach($sql as $row)
+	{
+		if(in_array($row['Field'], array('id', 'created_at', 'updated_at')))
+		{
+			$output .= "  - " . $row['Field'] . "\n";
+		}
+		else
+		{
+			//Separate the type and length
+			$type = explode("(", $row['Type']);
+			
+			// Output the row/collection indicator
+			$output .= "  - " . $row['Field'] . ":";
+			$output .= " { type: " . $type[0];
+			if($type[1]) $output .= ", length: " . str_replace(")", "", $type[1]);
+			$output .= ", allow_null: ";
+			if($row['Null'] == 'Yes') $output .= "true";
+			else $output .= "false";
+			$output .=  " }\n";
+		}
+	}
+
+	//Write to the yml file
+	$yml_file = $model_name_camel . '.yml';
+	$handle = fopen(dirname(__FILE__) . '/../config/schemas/' . $database_name . '/' . $yml_file, 'w') or die('Cannot open file:  '.$yml_file);
+	fwrite($handle, $output);
+
+	echo '\nYML file created...\n';
+}	
