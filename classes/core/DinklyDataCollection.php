3db2482117365703ee66a8bc79ad7c3d7fbef266
Uncle Cheese <unclecheese@leftandmain.com>
2013-10-18 11:03:01 +1300

    UPDATE: Move getAll() method into DinklyDataCollection. Deprecate BaseXXXCollection.

--- classes/core/DinklyDataCollection.php
+++ classes/core/DinklyDataCollection.php
@@ -12,15 +12,26 @@
 				if(self::fetchDB()->query($peer_object->getSelectQuery(), true)) { return self::handleResults($peer_object); }
 			}
 		}
-		
+
 	CHILD CLASS USAGE EXAMPLE:
-	
+
 		$users = UserCollection::getAll();
 
 ***************************************************************************************************************/
 
 abstract class DinklyDataCollection extends DinklyDataModel
-{ 
+{
+
+	public static function getAll()
+	{
+		$peer_class = preg_replace('/Collection$/', '', get_called_class());
+		if(class_exists($peer_class)) {
+			$peer_object = new $peer_class();
+			return self::getCollection($peer_object, $peer_object->getSelectQuery());
+		}
+	}
+
+
 	protected static function getCollection($peer_object, $query)
 	{
 		$db = self::fetchDB();
@@ -35,9 +46,9 @@ abstract class DinklyDataCollection extends DinklyDataModel
 				$class_name = get_class($peer_object);
 				$tempObject = new $class_name($db);
 				$tempObject->hydrate($result, true);
-				
+
 				$arrObject[$i] = $tempObject;
-				
+
 				$i++;
 			}
 			return $arrObject;
