301f87bc8fc2cdfb99c7e97a279ed1e78b2ed5fd
Scott Connor <scottconnor@bluehousegroup.com>
2014-03-24 12:41:29 -0400

    about to merge upstream fork

--- tools/unit_tests/classes/core/DinklyDataConfig.php
+++ tools/unit_tests/classes/core/DinklyDataConfig.php
@@ -7,10 +7,7 @@ class DinklyDataConfigTest extends PHPUnit_Framework_TestCase
 	protected function setUp()
 	{
 		date_default_timezone_set("Europe/Paris");
-		$dsn = 'mysql:dbname=admin;host=localhost;port=3306';
-		$username = 'root';
-		$password = 'root';
-		$this->db = new PDO($dsn, $username, $password);
+
 
 	}
 
@@ -20,6 +17,15 @@ class DinklyDataConfigTest extends PHPUnit_Framework_TestCase
 	public function testLoadDBCreds()
 	{
 		 // $this->assertTrue(true);
+		$dsn = 'mysql:dbname=admin;host=localhost;port=3306';
+		$username = 'root';
+		$password = 'root';
+		$this->db = new PDO($dsn, $username, $password);
+
+		$this->test_db = new PDO($dsn);
+		$this->test_db->fetchDB();
+
+		$this->assertEquals($this->db,$this->test_db);
 	}
 	
 
