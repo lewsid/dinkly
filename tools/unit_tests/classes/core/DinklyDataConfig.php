c0a014c2a68b26fe2c14631834530e4b8acc48b3
Scott Connor <scottconnor@bluehousegroup.com>
2014-03-24 15:13:29 -0400

    added more tests for DinklyDataConfig

--- tools/unit_tests/classes/core/DinklyDataConfig.php
+++ tools/unit_tests/classes/core/DinklyDataConfig.php
@@ -1,5 +1,5 @@
 <?php
-
+use Symfony\Component\Yaml\Yaml;
 class DinklyDataConfigTest extends PHPUnit_Framework_TestCase
 {
 		protected $db;
@@ -11,12 +11,44 @@ class DinklyDataConfigTest extends PHPUnit_Framework_TestCase
 
 	}
 
-	public function testLoadDBCreds()
+	public function testGetDBCreds()
 	{
-		 // $this->assertTrue(true);
-		$creds=DinklyDataConfig::getDBCreds();
+		//make sure the yaml is parsing correctly
+		$db_creds = Yaml::parse($_SERVER['APPLICATION_ROOT'] . "config/db.yml");
+		$creds = DinklyDataConfig::getDBCreds(); 
+		$this->assertEquals($db_creds['admin'],$creds);
+		
 	}
-	
+	public function testHasConnection(){
+
+				//test on connection that does not exist
+				$this->assertFalse(DinklyDataConfig::hasConnection('admin_table'));
+
+				//test create connection and then see if exists
+				DinklyDataConfig::loadDBCreds();
+				$this->assertTrue(DinklyDataConfig::hasConnection('admin'));
+
+
+	}
+		public function testSetActiveConnection(){
+
+		  $db_creds = Yaml::parse($_SERVER['APPLICATION_ROOT'] . "config/db.yml");  
+			$_SESSION['dinkly']['db_creds'] = $db_creds; 
+			$set_db=DinklyDataConfig::setActiveConnection('admin');
+			$this->assertTrue($set_db);
+			//make sure connection was actually made
+			$this->assertTrue(DinklyDataConfig::hasConnection('admin'));
+
+	}
+		public function testLoadDBCreds(){
+
+			$db_creds= DinklyDataConfig::getDBCreds(); 
+			$first_connection = key($db_creds);
+		
+
+
+
+	}
 
 }
 ?>
