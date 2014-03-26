72a48af749ade1c752e8588f8b92f3b153d1a4c9
Scott Connor <scottconnor@bluehousegroup.com>
2014-03-26 10:12:53 -0400

    new test functions for class AdminUser

--- tools/unit_tests/classes/models/custom/AdminUser.php
+++ tools/unit_tests/classes/models/custom/AdminUser.php
@@ -9,16 +9,27 @@ class AdminUserTest extends PHPUnit_Framework_TestCase
 		date_default_timezone_set("Europe/Paris");
 		$this->user= new AdminUser();
 		$this->user->init(1);
-		$_SESSION['dinkly']['current_app_name']="admin";
+		$_SESSION['dinkly']['current_app_name']='admin';
 	}
-
+	public function testGetLoggedUsername()
+	{
+		$test_username="scott";
+		$this->user->setLoggedIn(true,$test_username);
+		$this->assertEquals($this->user->getLoggedUsername(),$test_username);
+	}
 	public function testSetLoggedIn()
 	{
 		//make sure they are not logged in already
 		$this->assertFalse($this->user->isLoggedIn());
 		$username= $this->user->getUsername();
+		$val=true;
 		//if not logged in we can do so and check to make sure session vars are correct
-		
+		$this->user->setLoggedIn($val,$username);
+		$this->assertTrue($this->user->isLoggedIn());
+		$this->assertEquals($_SESSION['dinkly']['admin']['logged_in'],true);
+		$this->assertEquals($_SESSION['dinkly']['admin']['logged_username'],$username);
+		$this->assertEquals($_SESSION['dinkly']['admin']['logged_id'],$username);
+
 	}
 	public function testIsLoggedIn()
 	{
