961dd9aa2a68bf6e76129c66123f386aeedb563d
Christopher Lewis <lewsid@lewsid.com>
2014-04-07 16:03:40 +0200

    unit test error fix

--- tools/run_unit_tests.php
+++ tools/run_unit_tests.php
@@ -1,6 +1,6 @@
 <?php
 
-system('php vendor/phpunit/phpunit/phpunit --bootstrap config/bootstrap.php tools/unit_tests/classes/core/DinklyBase.php');
+system('php vendor/phpunit/phpunit/phpunit --bootstrap config/bootstrap.php tools/unit_tests/classes/core/DinklyBase.php --colors');
 system('php vendor/phpunit/phpunit/phpunit --bootstrap config/bootstrap.php tools/unit_tests/classes/core/DinklyBuilder.php');
 system('php vendor/phpunit/phpunit/phpunit --bootstrap config/bootstrap.php tools/unit_tests/classes/core/DinklyDataCollection.php');
 system('php vendor/phpunit/phpunit/phpunit --bootstrap config/bootstrap.php tools/unit_tests/classes/core/DinklyDataConfig.php');
