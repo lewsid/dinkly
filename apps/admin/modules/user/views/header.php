da145d7e37ab67cfba110deb248f18b5d38795cb
Scott Connor <scottconnor@bluehousegroup.com>
2014-04-14 15:57:23 -0400

    added data tables assetts, but still need some styling

--- apps/admin/modules/user/views/header.php
+++ apps/admin/modules/user/views/header.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="/css/datatables-bootstrap3.css">
 <script type="text/javascript" src="/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="/js/dataTables.bootstrap3.js"></script>
+<script type="text/javascript" src="/js/dataTables.editor.bootstrap3.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
 	/* Table initialisation */
