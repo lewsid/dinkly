b1ecf40a1cdff6ddfea3a4058903632657ebb509
Scott Connor <scottconnor@bluehousegroup.com>
2014-04-14 14:46:13 -0400

    styled main header

--- apps/admin/layout/header.php
+++ apps/admin/layout/header.php
@@ -8,12 +8,16 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Le styles -->
-    <link href="/css/bootstrap.css" rel="stylesheet">
+    <!-- <link href="/css/bootstrap.css" rel="stylesheet"> -->
+    <link href="/css/bootstrap-theme.css" rel="stylesheet">
+    <link href="/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="/css/bootstrap3.css" rel="stylesheet">
+    <link href="/css/bootstrap3.min.css" rel="stylesheet">
     <style>
       body { padding-top: 60px; /* 60px to make the container go all the way
       to the bottom of the topbar */ }
     </style>
-    <link href="/css/bootstrap-responsive.css" rel="stylesheet">
+ <!--    <link href="/css/bootstrap-responsive.css" rel="stylesheet"> -->
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
@@ -27,7 +31,8 @@
     <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
     
     <script type="text/javascript" src="/js/jquery-1.9.1.min.js"></script>
-    <script type="text/javascript" src="/js/bootstrap.js"></script>
+   <!--  // <script type="text/javascript" src="/js/bootstrap.js"></script> -->
+   <script type="text/javascript" src="/js/bootstrap3.js"></script>
 
     <script type="text/javascript">
     $("#sign-in").click(function() {
@@ -39,7 +44,62 @@
 
   </head>
   <body>
-    <div class="navbar navbar-fixed-top navbar-inverse">
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">
+            <?php echo Dinkly::getConfigValue('app_name'); ?>
+          </a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+              <li>
+                  <a href="/">
+                    Home
+                  </a>
+              </li>
+            </ul>
+          <form class="navbar-form navbar-right" role="form">
+            <?php if(AdminUser::isLoggedIn()): ?>
+            <li>
+              <a href="/user/user_list/">
+                User List
+              </a>
+            </li>
+            <?php endif; ?>
+            <?php if(AdminUser::isLoggedIn()): ?>
+            <li>
+              <a href="/login/logout/">
+                Logout
+              </a>
+            </li>
+            <?php endif; ?>
+          </ul>
+          <?php if(!AdminUser::isLoggedIn()): ?>
+          <form id="sign-in-form" class="navbar-form pull-right" action="/login/" method="post">
+            <div class="form-group">
+              <input name="username" type="text" placeholder="Username" class="form-control">
+            </div>
+            <div class="form-group">
+             <input name="password" type="password" placeholder="Password" class="form-control">
+            </div>
+            <div class="form-group">
+              <button class="btn btn-success" id="sign-in">
+                Sign in
+              </button>
+            </div>
+          </form>
+          <?php endif; ?>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>
+<!--     <div class="navbar navbar-fixed-top navbar-inverse">
       <div class="navbar-inner">
         <div class="container">
           <a class="brand" href="#">
@@ -77,7 +137,7 @@
           <?php endif; ?>
         </div>
       </div>
-    </div>
+    </div> -->
     <div class="container">
       <?php if(isset($_SESSION['dinkly']['badlogin'])): ?>
       <div class="alert alert-error">Invalid login</div>
