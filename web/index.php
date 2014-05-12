c53a30348c1edc3d46a137fbed2dc726f7a509a1
Christopher Lewis <lewsid@lewsid.com>
2014-05-12 18:17:37 +0200

    removed session hack

--- web/index.php
+++ web/index.php
@@ -6,6 +6,4 @@ require_once('../config/bootstrap.php');
 
 if(isset($_GET['nocache'])) { session_destroy(); header("Location: /"); }
 
-if(isset($_GET['showsession'])) { echo '<pre>'; print_r($_SESSION); die(); }
-
 $Dinkly->route($_SERVER['REQUEST_URI']);
