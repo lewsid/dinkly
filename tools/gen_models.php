31ec19424d8f5edfe1b7b2587a7895ee3c04e62b
Christopher Lewis <lewsid@lewsid.com>
2014-08-25 14:44:51 +0200

    super minor extra variable removed

--- tools/gen_models.php
+++ tools/gen_models.php
@@ -2,7 +2,7 @@
 
 require_once('config/bootstrap.php');
 
-$sql = $target_connection = $schema = null;
+$sql = $schema = null;
 $options = getopt("s:i");
 if(isset($options['i'])) { $sql = true; }
 if(isset($options['s'])) { $schema = $options['s']; }
