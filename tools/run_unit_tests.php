bbfceedf060796c0f4955d0a1f437501c7a85f1d
Elliott Asher <elliott250@gmail.com>
2015-03-06 08:57:21 -0500

    Revert "fixed run_unit_test.php system commands"
    This reverts commit dac7b56be5f32296cb6c98d72f15a65037017a33.
    
    Conflicts:
    	tools/run_unit_tests.php

--- tools/run_unit_tests.php
+++ tools/run_unit_tests.php
@@ -5,5 +5,9 @@ system('php vendor/phpunit/phpunit/phpunit --bootstrap config/bootstrap.php tool
 system('php vendor/phpunit/phpunit/phpunit --bootstrap config/bootstrap.php tools/unit_tests/classes/core/DinklyDataCollection.php');
 system('php vendor/phpunit/phpunit/phpunit --bootstrap config/bootstrap.php tools/unit_tests/classes/core/DinklyDataConfig.php');
 system('php vendor/phpunit/phpunit/phpunit --bootstrap config/bootstrap.php tools/unit_tests/classes/core/DinklyDataConnector.php');
+<<<<<<< HEAD
 system('php vendor/phpunit/phpunit/phpunit --bootstrap config/bootstrap.php tools/unit_tests/classes/core/DinklyDataModel.php');
 system('php vendor/phpunit/phpunit/phpunit --bootstrap config/bootstrap.php tools/unit_tests/classes/core/DinklyFlash.php');
+=======
+system('php vendor/phpunit/phpunit/phpunit --bootstrap config/bootstrap.php tools/unit_tests/classes/core/DinklyDataModel.php');
+>>>>>>> parent of dac7b56... fixed run_unit_test.php system commands
