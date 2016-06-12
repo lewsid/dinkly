123776009d1c6b6afef48fb29a8ca09faf3da7ca
Christopher Lewis <lewsid@lewsid.com>
2016-06-12 13:49:48 -0400

    path tweak

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -5,6 +5,6 @@ date_default_timezone_set("America/New_York");
 $_SERVER['APPLICATION_ROOT'] = dirname(__FILE__) . '/../';
 
 require_once $_SERVER['APPLICATION_ROOT'] . 'vendor/autoload.php';
-require_once $_SERVER['APPLICATION_ROOT'] . '/config/autoload.php';
+require_once $_SERVER['APPLICATION_ROOT'] . 'config/autoload.php';
 
 $Dinkly = new Dinkly('dev');
