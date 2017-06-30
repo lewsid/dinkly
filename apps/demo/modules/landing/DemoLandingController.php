cdf374179d39622fc43db23e79c06785b1b11d25
Christopher Lewis <lewsid@lewsid.com>
2017-06-30 14:55:53 -0400

    additional references to parameters array removed

--- apps/demo/modules/landing/DemoLandingController.php
+++ apps/demo/modules/landing/DemoLandingController.php
@@ -21,7 +21,7 @@ class DemoLandingController extends DemoController
 	 * @return bool: always returns true on successful construction of view
 	 *
 	 */
-	public function loadDefault($parameters = array())
+	public function loadDefault()
 	{
 		return true;
 	}
