74ae3eb81d6749a2d72f38e25342722fe3c94633
Christopher Lewis <lewsid@lewsid.com>
2018-01-10 16:54:07 -0500

    slight style tweaks

--- apps/demo/layout/footer.php
+++ apps/demo/layout/footer.php
@@ -1,7 +1,3 @@
 <?php echo $this->getModuleFooter(); ?>
-<script type="text/javascript" src="/js/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="/js/bootstrap.4.0.0-beta.3.min.js"></script>
-<script type="text/javascript" src="/js/datatables.1.10.16.combined.min.js"></script>
-<script type="text/javascript" src="/js/dinkly.3.29.js"></script>
 </body>
 </html>
